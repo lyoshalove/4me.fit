1708b7d87c93536fff72f1940d9f01c387375e16
yungromantic <78252352+yungromantic@users.noreply.github.com>
2022-04-06 18:48:12 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
 
   //Burger
   $('.burger').on('click', function() {
@@ -44,133 +42,138 @@ $(document).ready(function () {
       },
     ],
   });
-});
 
-let reviewsText = [];
 
-$(".reviews__slider-item").each((i, slide) => {
-  const textElem = $(slide).find(".reviews__slider-descr");
-  const textValue = textElem.text();
-  const textLength = textValue.length;
+  let reviewsText = [];
 
-  if (textLength > 250) {
-    reviewsText[i] = textValue;
+  $(".reviews__slider-item").each((i, slide) => {
+    const textElem = $(slide).find(".reviews__slider-descr");
+    const textValue = textElem.text();
+    const textLength = textValue.length;
 
-    const croppedText = textValue.substr(0, 250);
-    const indexLastSpace = croppedText.lastIndexOf(" ");
+    if (textLength > 250) {
+      reviewsText[i] = textValue;
 
-    textElem.text(croppedText.slice(0, indexLastSpace) + " ");
-    textElem.append('<a href="#" class="reviews__slider-more more"> ... Далее</a>');
-  }
-});
+      const croppedText = textValue.substr(0, 250);
+      const indexLastSpace = croppedText.lastIndexOf(" ");
 
-$(".reviews__slider").on("click", ".reviews__slider-more", function (e) {
-  e.preventDefault();
-
-  const slide = $(this).parents(".reviews__slider-item");
-  const slideIndex = slide.index();
-  console.log(slideIndex);
-  const textElem = slide.find(".reviews__slider-descr");
-  const heightBefore = textElem.height();
-
-  textElem.text(reviewsText[slideIndex]);
-  const heightAfter = textElem.height();
-
-  textElem.css("height", heightBefore + "px");
-
-  setTimeout(() => textElem.css("height", heightAfter + "px"));
-  setTimeout(() => textElem.css("height", "auto"), 500);
-});
-
-let advantagesText = [];
-
-$(".advantages__item").each((i, slide) => {
-  const textElem = $(slide).find(".advantages__item-descr");
-  const textValue = textElem.text();
-  const textLength = textValue.length;
-
-  if (textLength > 250) {
-    advantagesText[i] = textValue;
-
-    const croppedText = textValue.substr(0, 250);
-    const indexLastSpace = croppedText.lastIndexOf(" ");
-
-    textElem.text(croppedText.slice(0, indexLastSpace) + " ");
-    textElem.append('<a href="#" class="advantages__item-more more"> ... Далее</a>');
-  }
-});
-
-$(".advantages__items").on("click", ".advantages__item-more", function (e) {
-  e.preventDefault();
-
-  const slide = $(this).parents(".advantages__item");
-  const slideIndex = slide.index();
-  console.log(slideIndex);
-  const textElem = slide.find(".advantages__item-descr");
-  const heightBefore = textElem.height();
-
-  textElem.text(advantagesText[slideIndex]);
-  const heightAfter = textElem.height();
-
-  textElem.css("height", heightBefore + "px");
-
-  setTimeout(() => textElem.css("height", heightAfter + "px"));
-  setTimeout(() => textElem.css("height", "auto"), 500);
-});
-
-//Accordion
-function addAccordion(items, item,  text) {
-  $(items).on("click", item, function (e) {
-    const target = e.currentTarget;
-
-    $(target).toggleClass("active");
-    $(target).find(text).slideToggle("active");
+      textElem.text(croppedText.slice(0, indexLastSpace) + " ");
+      textElem.append(
+        '<a href="#" class="reviews__slider-more more"> ... Далее</a>'
+      );
+    }
   });
-}
 
-addAccordion(".faq__items", ".faq__item", ".faq__item-text");
+  $(".reviews__slider").on("click", ".reviews__slider-more", function (e) {
+    e.preventDefault();
 
-// Validate forms
-$(".subscribe__form").validate({
-  rules: {
-    email: {
-      email: true,
-    },
-  },
-  messages: {
-    email: {
-      email: "Введите email",
-      required: "Это обязательное поле email",
-    },
-  },
-  // callback submit form
-  submitHandler: function () {},
-});
+    const slide = $(this).parents(".reviews__slider-item");
+    const slideIndex = slide.index();
+    console.log(slideIndex);
+    const textElem = slide.find(".reviews__slider-descr");
+    const heightBefore = textElem.height();
 
-//Show All
+    textElem.text(reviewsText[slideIndex]);
+    const heightAfter = textElem.height();
 
-let counter = 12;
-const trainingItems = $(".training__card");
+    textElem.css("height", heightBefore + "px");
 
-if(window.innerWidth <= 480) {
-  counter = 4;
+    setTimeout(() => textElem.css("height", heightAfter + "px"));
+    setTimeout(() => textElem.css("height", "auto"), 500);
+  });
 
-  showItems(trainingItems, counter);
-} else {
-  showItems(trainingItems, counter);
-}
+  let advantagesText = [];
 
-function showItems(items, count) {
-  const parent = $(items).parent().text("");
-  for (let i = 0; i < count; i++) {
-    parent.append(items[i]);
+  $(".advantages__item").each((i, slide) => {
+    const textElem = $(slide).find(".advantages__item-descr");
+    const textValue = textElem.text();
+    const textLength = textValue.length;
+
+    if (textLength > 250) {
+      advantagesText[i] = textValue;
+
+      const croppedText = textValue.substr(0, 250);
+      const indexLastSpace = croppedText.lastIndexOf(" ");
+
+      textElem.text(croppedText.slice(0, indexLastSpace) + " ");
+      textElem.append(
+        '<a href="#" class="advantages__item-more more"> ... Далее</a>'
+      );
+    }
+  });
+
+  $(".advantages__items").on("click", ".advantages__item-more", function (e) {
+    e.preventDefault();
+
+    const slide = $(this).parents(".advantages__item");
+    const slideIndex = slide.index();
+    console.log(slideIndex);
+    const textElem = slide.find(".advantages__item-descr");
+    const heightBefore = textElem.height();
+
+    textElem.text(advantagesText[slideIndex]);
+    const heightAfter = textElem.height();
+
+    textElem.css("height", heightBefore + "px");
+
+    setTimeout(() => textElem.css("height", heightAfter + "px"));
+    setTimeout(() => textElem.css("height", "auto"), 500);
+  });
+
+  //Accordion
+  function addAccordion(items, item, text) {
+    $(items).on("click", item, function (e) {
+      const target = e.currentTarget;
+
+      $(target).toggleClass("active");
+      $(target).find(text).slideToggle("active");
+    });
   }
-}
 
-$(".training__btn ").on('click', function(e) {
-  e.preventDefault();
+  addAccordion(".faq__items", ".faq__item", ".faq__item-text");
 
-  counter = trainingItems.length;
-  showItems(trainingItems, counter);
-  $(this).hide();
+  // Validate forms
+  $(".subscribe__form").validate({
+    rules: {
+      email: {
+        email: true,
+      },
+    },
+    messages: {
+      email: {
+        email: "Введите email",
+        required: "Это обязательное поле email",
+      },
+    },
+    // callback submit form
+    submitHandler: function () {},
+  });
+
+  //Show All
+
+  let counter = 12;
+  const trainingItems = $(".training__card");
+
+  if (window.innerWidth <= 480) {
+    counter = 4;
+
+    showItems(trainingItems, counter);
+  } else {
+    showItems(trainingItems, counter);
+  }
+
+  function showItems(items, count) {
+    const parent = $(items).parent().text("");
+    for (let i = 0; i < count; i++) {
+      parent.append(items[i]);
+    }
+  }
+
+  $(".training__btn ").on("click", function (e) {
+    e.preventDefault();
+
+    counter = trainingItems.length;
+    showItems(trainingItems, counter);
+    $(this).hide();
+  });
 });
